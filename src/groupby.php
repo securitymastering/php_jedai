fd4ecdba4b3c9b674378922be2807a8f2ce68591
Rafael Ferreira Mendes <sevendays20@hotmail.com>
2023-11-05 03:00:11 -0300

    update

--- src/groupby.php
+++ src/groupby.php
@@ -2,13 +2,11 @@
 
 include('../sql/db_conexao.php');
 
-$sql = $pdo->prepare("SELECT * FROM `clientes` LEFT JOIN `cargo` ON `clientes`.`cargo` = `cargos`.`id`");
+$sql = $pdo->prepare("SELECT * FROM `clientes` GROUP BY `cargo` ORDER BY nome ASC LIMIT 2");
 $sql->execute();
 $clientes = $sql->fetchAll();
 foreach($clientes as $key => $value){
     echo $value['nome'];
-    echo '|';
-    echo $value['nome_cargo'];
     echo '<hr>';
 }
 ?>
